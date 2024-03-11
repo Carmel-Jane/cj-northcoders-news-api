138f4b63576521cd36f6ae47028278ddadc108b3
carmeljane <carmelbarwick@gmail.com>
2024-03-11 10:51:17 +0000

    installed cors, and required and used cors in app.js file

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ const {
   handleServerErrors,
   handlePsqlErrors,
 } = require("./errors");
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
